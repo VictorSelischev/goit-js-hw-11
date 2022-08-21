80d75e4bd1be33f2130946ea64206060590a6d0b
Victor Selischev <selischev.victor.kharkov@gmail.com>
2022-08-21 15:04:55 +0300

    version 1.0

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 // import axios from 'axios';
 // const axios = require('axios').default;
 import Notiflix from 'notiflix';
-import SimpleLightbox from "simplelightbox";
 
 const formRef = document.querySelector('#search-form');
 // const btnRef = document.querySelector('.search-form__button');
